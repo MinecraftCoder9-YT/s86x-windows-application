55156727c8287f8874bb816591108727302a76d5
MinecraftCoder9-YT <104759571+MinecraftCoder9-YT@users.noreply.github.com>
2022-05-02 10:12:36 +0100

    Added download.bat

---
+++ download.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Welcome to s86x-windows.
+echo use Ctrl-C to exit or click enter to continue
+REM this will start application
+echo IMPORTANT NOTICE.
+echo please install Python from python.org/downloads
+pause
